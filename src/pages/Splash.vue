b3d0f231a8ef8b4d60ded970d7fe40e9a7055e40
alex <15821745523@163.com>
2018-05-15 10:58:11 +0800

    brand

--- src/pages/Splash.vue
+++ src/pages/Splash.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="splash">
      <img class="splash-img" src="../../static/fisherman.png">
+     <div class="version">
+         <p> 
+             <span>
+                 <i>FisherMan</i>
+            </span>
+         </p>   
+         <p>©Google Inc.</p>
+     </div>
   </div>
 </template>
 <script>
@@ -25,15 +33,32 @@ export default {
               document.getElementsByClassName("splash")[0].style.height = this.getViewportSize().height+"px";
         })
         setTimeout(()=>{
-            this.$router.push("login");
+             this.$router.push("login");
         },3000)
     }
   }
 </script>
 <style lang="stylus" scoped>
     .splash
+        position:relative;
         .splash-img
             display:block;
             width:100%;
             height:100%
+        .version
+            width:100%;
+            position:fixed;
+            bottom:2rem;
+            p
+                span 
+                        display: block;
+                        border: .2rem solid #fff;
+                        width: 10rem;
+                        margin: 0 auto;
+                        border-radius:.4rem;
+                margin:0
+                padding:1rem 0;
+                text-align :center;
+                color:#fff;
+                font-size:1.6rem;
 </style>
